a4bf044c872f87909bfe4a46989d7a28690308c3
melvinthemok <melvinmok.cy@gmail.com>
2017-02-02 14:03:32 +0800

    Complete exercise

--- lib/digital_item.rb
+++ lib/digital_item.rb
@@ -1,1 +1,17 @@
-# You are on your own... good luck.
+require_relative 'item.rb'
+
+class DigitalItem < Item
+
+  def initialize(name, price, quantity)
+    super(name, price)
+    @quantity = 1
+  end
+
+  def sell(amount)
+
+  end
+
+  def stock(amount)
+    
+  end
+end
